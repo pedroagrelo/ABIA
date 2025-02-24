65a7de02354c8ad432cd219046d36006bcccf413
pedroagrelo <pedro.agrelo.marquez@gmail.com>
2025-02-24 13:05:40 +0100

    profundidad 1º opcion

--- obj/Debug/net8.0/PracticaSemana1.AssemblyInfo.cs
+++ obj/Debug/net8.0/PracticaSemana1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PracticaSemana1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8836ce19b04b607e7ca5bd4bd3c6db0b72bfa794")]
 [assembly: System.Reflection.AssemblyProductAttribute("PracticaSemana1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PracticaSemana1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
