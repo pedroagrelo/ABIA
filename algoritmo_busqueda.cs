cd9a99e54c19d3e98d4af212a52d34ab6de3ab46
pedroagrelo <pedro.agrelo.marquez@gmail.com>
2025-03-01 15:03:06 +0100

    entrega semana2

--- algoritmo_busqueda.cs
+++ algoritmo_busqueda.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-
-
 namespace PRACTICASEMANA1;
 
 // Clase abstracta que define la estructura básica para un algoritmo de búsqueda
@@ -78,5 +76,25 @@ public class BusquedaEnProfundidad : AlgoritmoDeBusqueda
     }
 }
 
+/// <summary>
+/// Implementación de la búsqueda en anchura, que expande nodos en orden FIFO.
+/// </summary>
+public class BusquedaAnchura : AlgoritmoDeBusqueda
+{
+    /// <summary>
+    /// Constructor que inicializa la búsqueda en anchura con una cola FIFO.
+    /// </summary>
+    public BusquedaAnchura() : base(new ColaFIFO()) {}
 
+    /// <summary>
+    /// En búsqueda en anchura, todos los nodos tienen la misma prioridad.
+    /// </summary>
+    /// <param name="nodoInfo">El nodo cuya prioridad se calcula.</param>
+    /// <param name="calculoHeuristica">Función heurística opcional (no utilizada en anchura).</param>
+    /// <returns>Siempre devuelve 0, ya que no se usa heurística.</returns>
+    public override int CalculoDePrioridad(Solucion nodoInfo, Func<Solucion, int>? calculoHeuristica = null)
+    {
+        return 0; // En anchura no hay prioridad diferenciada, todos los nodos se procesan FIFO.
+    }
+}
 
--- obj/Debug/net8.0/PracticaSemana1.AssemblyInfo.cs
+++ obj/Debug/net8.0/PracticaSemana1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PracticaSemana1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e374fb19478b59ca2cacfb994a3bd6cdd6aaa7e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65ca5cdb248be6fcb181d6116fcf022455f818d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PracticaSemana1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PracticaSemana1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
